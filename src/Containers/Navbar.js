5ccf485872d1368b3a6db59880a108291d0bc1b2
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-09-10 10:30:21 -0400

    redirect to frontpage on logout finally working, also properly centered welcome text

--- src/Containers/Navbar.js
+++ src/Containers/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { NavLink} from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 
 class Navbar extends React.Component {
   
   //refactor to just one function!
   renderLoginLogout = () => {
-    return localStorage.getItem("token") ? <Menu.Item onClick={this.props.logout} position="right">Log out</Menu.Item> : <Menu.Item as={NavLink} to="/login" position="right">Login</Menu.Item>
+    return localStorage.getItem("token") ? <Menu.Item as={Link} to="/" onClick={this.props.logout} position="right">Log out</Menu.Item> : <Menu.Item as={NavLink} to="/login" position="right">Login</Menu.Item>
   }
   renderMyPage = () => {
     return localStorage.getItem("token") ? <Menu.Item as={NavLink} exact to="/my-page">My Bots</Menu.Item> : <Menu.Item as={NavLink} exact to="/create">Create</Menu.Item>
